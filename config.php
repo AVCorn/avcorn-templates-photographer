385e73552aa34bed50ac6c59d4e0e7943d63460e
Benjamin J. Young <ben@blaher.me>
2024-01-11 12:08:04 -0500

    Adding initial files

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * New Template Configuration
+ *
+ * PHP version 8.2
+ *
+ * @phpversion >= 8.2
+ * @category   CMS
+ * @package    AVCorn
+ * @subpackage Template
+ * @author     Benjamin J. Young <ben@blaher.me>
+ * @license    GNU General Public License, version 3
+ * @link       https://github.com/avcorn/avcorn
+ */
+
+declare(strict_types=1);
+
+/**
+ * Overriding default configuration.
+ *
+ * @var array $config Configuration
+ */
+$config['info']['company'] = "Nutty Photography";
+$config['info']['title']   = 'Hello, my name<br /> is Nutty';
+$config['home']            = '/home/photographer';
